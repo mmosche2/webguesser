514a36e260c9630aadedf385df46f809c272a656
Mark <mark.moschel@gmail.com>
2013-07-17 19:16:02 -0500

    I3: add a check_guess method

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number = rand(100)
+RANDOM_NUMBER = rand(100)
 
 get '/' do
-	guess = params[:guess].to_i
-  erb :index, :locals => {:random_number => random_number, guess: guess}
+	message = check_guess(params[:guess].to_i) if params[:guess]
+  erb :index, :locals => {:random_number => RANDOM_NUMBER, message: message}
+end
+
+def check_guess(guess)
+	case
+	when guess == RANDOM_NUMBER then "You got it right! The secret number is #{RANDOM_NUMBER}"
+	when guess > RANDOM_NUMBER+5 then "Way too high"
+	when guess > RANDOM_NUMBER then "Too high"
+	when guess < RANDOM_NUMBER-5 then "Way too low"
+	when guess < RANDOM_NUMBER then "Too low"
+	end
 end
