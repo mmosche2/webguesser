6e1d9b8ec6f7ff266772b879fe67a298777ff2e1
Mark <mark.moschel@gmail.com>
2013-07-17 19:26:46 -0500

    Extension: add color coding to guess response

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,16 +4,21 @@ require 'sinatra/reloader'
 RANDOM_NUMBER = rand(100)
 
 get '/' do
-	message = check_guess(params[:guess].to_i) if params[:guess]
-  erb :index, :locals => {:random_number => RANDOM_NUMBER, message: message}
+	message, css_color = check_guess(params[:guess].to_i) if params[:guess]
+  erb :index, :locals => {:random_number => RANDOM_NUMBER, message: message, css_color: css_color}
 end
 
 def check_guess(guess)
 	case
-	when guess == RANDOM_NUMBER then "You got it right! The secret number is #{RANDOM_NUMBER}"
-	when guess > RANDOM_NUMBER+5 then "Way too high"
-	when guess > RANDOM_NUMBER then "Too high"
-	when guess < RANDOM_NUMBER-5 then "Way too low"
-	when guess < RANDOM_NUMBER then "Too low"
+	when guess == RANDOM_NUMBER 
+		["You got it right! The secret number is #{RANDOM_NUMBER}", "green"]
+	when guess > RANDOM_NUMBER+5 
+		["Way too high", "red"]
+	when guess > RANDOM_NUMBER 
+		["Too high", "#FFDDDD"]
+	when guess < RANDOM_NUMBER-5 
+		["Way too low", "red"]
+	when guess < RANDOM_NUMBER 
+		["Too low", "#FFDDDD "]
 	end
 end
