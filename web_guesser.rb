4b5da69dfe2a917be8bcd5b5b6dee14d393bfd88
Mark <mark.moschel@gmail.com>
2013-07-17 20:37:59 -0500

    add cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,25 +3,31 @@ require 'sinatra/reloader'
 
 RANDOM_NUMBER = rand(100)
 @@remaining_guesses = 5
+@@correct = false
 
 get '/' do
-	if @@remaining_guesses == 0
+	cheat_mode = params[:cheat]
+	message, css_color = check_guess(params[:guess].to_i) if params[:guess]
+	if @@remaining_guesses == 1 && !@@correct
 		RANDOM_NUMBER = rand(100)
 		@@remaining_guesses = 5
-		# display lost message
-		erb :index, :locals => {:random_number => RANDOM_NUMBER, message: "you lost", css_color: "red"}
-	else 
-		@@remaining_guesses = @@remaining_guesses - 1
-		message, css_color = check_guess(params[:guess].to_i) if params[:guess]
-		RANDOM_NUMBER = rand(100) if @@remaining_guesses == 5
-  	erb :index, :locals => {:random_number => RANDOM_NUMBER, message: message, css_color: css_color}
+		message = "you lost"
+		css_color = "red"
+	elsif @@correct
+		RANDOM_NUMBER = rand(100)
+		@@correct = false
+		@@remaining_guesses = 5
+	else #not last turn and got it wrong
+		@@remaining_guesses -= 1
   end
+  erb :index, :locals => {:random_number => RANDOM_NUMBER, message: message, css_color: css_color,
+  													cheat_mode: cheat_mode, count: @@remaining_guesses}
 end
 
 def check_guess(guess)
 	case
 	when guess == RANDOM_NUMBER 
-		@@remaining_guesses = 5
+		@@correct = true
 		["You got it right! The secret number is #{RANDOM_NUMBER}", "green"]
 	when guess > RANDOM_NUMBER+5 
 		["Way too high", "red"]
